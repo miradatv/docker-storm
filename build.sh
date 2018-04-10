a6c68137cec6cc88ba8968a34f3c4196a6042182
Javier Arriero <javier.arriero@mirada.tv>
2018-04-10 15:16:45 +0200

    Update to Storm 1.0.5

--- build.sh
+++ build.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
-VERSION=1.0.1
+VERSION=1.0.5
 
-docker build -t miradatv/storm-base base
-docker build -t miradatv/storm-nimbus nimbus
-docker build -t miradatv/storm-worker worker
-docker build -t miradatv/storm-ui ui
-docker tag miradatv/storm-base:latest miradatv/storm-base:$VERSION
-docker tag miradatv/storm-nimbus:latest miradatv/storm-nimbus:$VERSION
-docker tag miradatv/storm-worker:latest miradatv/storm-worker:$VERSION
-docker tag miradatv/storm-ui:latest miradatv/storm-ui:$VERSION
-docker push miradatv/storm-base:$VERSION
-docker push miradatv/storm-base:latest
-docker push miradatv/storm-nimbus:$VERSION
-docker push miradatv/storm-nimbus:latest
-docker push miradatv/storm-worker:$VERSION
-docker push miradatv/storm-worker:latest
-docker push miradatv/storm-ui:$VERSION
-docker push miradatv/storm-ui:latest
+export ECR=606039442951.dkr.ecr.us-east-1.amazonaws.com
+
+docker build -t storm-base base --no-cache
+docker build -t storm-nimbus nimbus --no-cache
+docker build -t storm-worker worker --no-cache
+docker build -t storm-ui ui --no-cache
+docker tag storm-base $ECR/storm-base:$VERSION
+docker tag storm-nimbus $ECR/storm-nimbus:$VERSION
+docker tag storm-worker $ECR/storm-worker:$VERSION
+docker tag storm-ui $ECR/storm-ui:$VERSION
+docker tag storm-base $ECR/storm-base:latest
+docker tag storm-nimbus $ECR/storm-nimbus:latest
+docker tag storm-worker $ECR/storm-worker:latest
+docker tag storm-ui $ECR/storm-ui:latest
+docker push $ECR/storm-base:$VERSION
+docker push $ECR/storm-base:latest
+docker push $ECR/storm-nimbus:$VERSION
+docker push $ECR/storm-nimbus:latest
+docker push $ECR/storm-worker:$VERSION
+docker push $ECR/storm-worker:latest
+docker push $ECR/storm-ui:$VERSION
+docker push $ECR/storm-ui:latest
 
